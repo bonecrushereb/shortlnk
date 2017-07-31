821505625bce3e6c5295132aea7cfd06c4e4f60d
bonecrushereb <bonecrushereb@gmail.com>
2017-07-31 09:31:06 -0700

    add routing

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,9 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
 
+const routes = (
+  <Router>
+    <div>
+      <Route path="/signup" component={Signup}/>
+      <Route path="/links" component={Link}/>
+    </div>
+</Router>
+);
+
 Meteor.startup(() => {
-  ReactDOM.render(<Link/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
