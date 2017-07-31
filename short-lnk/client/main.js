1b7a0192c0caca26a25e190437954255baee7912
bonecrushereb <bonecrushereb@gmail.com>
2017-07-31 10:08:38 -0700

    add login and notfound components routing successful

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,17 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
+import NotFound from '../imports/ui/NotFound';
+import Login from '../imports/ui/Login';
 
 const routes = (
   <Router>
-    <div>
-      <Route path="/signup" component={Signup}/>
-      <Route path="/links" component={Link}/>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Login}/>
+      <Route exact path="/signup" component={Signup}/>
+      <Route exact path="/links" component={Link}/>
+      <Route component={NotFound}/>
+    </Switch>
 </Router>
 );
 
