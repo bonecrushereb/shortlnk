a746166c0d2fd61ae1a31684db14b2a354f9b358
bonecrushereb <bonecrushereb@gmail.com>
2017-08-02 11:12:32 -0700

    work on redirecting

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -2,22 +2,35 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Tracker } from 'meteor/tracker';
 
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
 import NotFound from '../imports/ui/NotFound';
 import Login from '../imports/ui/Login';
 
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/links'];
 const routes = (
   <Router>
     <Switch>
-      <Route exact path="/" component={Login}/>
+      <Route exact path="/" compoent={Login}/>
       <Route exact path="/signup" component={Signup}/>
       <Route exact path="/links" component={Link}/>
       <Route component={NotFound}/>
     </Switch>
 </Router>
 );
+
+// Tracker.autorun(() => {
+//   const isAuthenticated = !!Meteor.userId();
+//   const pathname = location.pathname;
+//   const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+//   const isAuthenticatedPage = authenticatedPages.includes(pathname);
+//
+//   console.log('pathname', pathname);
+//   console.log('isAuthenticated', isAuthenticated);
+// });
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
