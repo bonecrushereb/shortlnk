a76a9d2623507ddc4b09d512e28ca2fe142052ab
bonecrushereb <bonecrushereb@gmail.com>
2017-08-05 11:48:45 -0700

    work on link component

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,7 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
-
+import { Tracker } from 'meteor/tracker'
 import { routes } from '../imports/routes/routes';
+import { Links } from '../imports/api/links';
+
+Tracker.autorun(() => {
+  let links = Links.find().fetch();
+  console.log(links);
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
--- short-lnk/imports/ui/Link.js
+++ short-lnk/imports/ui/Link.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Accounts } from 'meteor/accounts-base';
+import { Links } from '../api/links';
 
 
 export class Link extends React.Component {
   onLogout() {
     Accounts.logout();
   }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const url = this.refs.url.value.trim();
+
+    if (url) {
+      Links.insert({ url });
+      this.refs.url.value = '';
+    }
+  }
   render() {
     return (
       <div>
         <h1>Your Links</h1>
-      <button onClick={this.onLogout.bind(this)}>Logout</button>
+        <button onClick={this.onLogout.bind(this)}>Logout</button>
+        <p>Add Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="text" ref="url" placeholder="URL"/>
+          <button>Add Links</button>
+        </form>
       </div>
     );
   }
