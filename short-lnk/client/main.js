b95424b4748ec6f0238001fe26919fab4d0e616e
bonecrushereb <bonecrushereb@gmail.com>
2017-08-09 12:32:08 -0700

    render links to page

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,13 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
-import { Tracker } from 'meteor/tracker'
 import { routes } from '../imports/routes/routes';
-import { Links } from '../imports/api/links';
 
-Tracker.autorun(() => {
-  let links = Links.find().fetch();
-  console.log(links);
-});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
--- short-lnk/imports/ui/LinksList.js
+++ short-lnk/imports/ui/LinksList.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { Tracker } from 'meteor/tracker'
+import { Links } from '../api/links';
 
 export default class LinksList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      links: []
+    };
+  }
   componentDidMount() {
-    console.log('componentDidMount LinksList');
+    Tracker.autorun(() => {
+      const links = Links.find().fetch();
+      this.setState({ links });
+    });
   }
   componentWillUnmount() {
     console.log('componentWillUnmount');
+  }
+  renderLinksListItems() {
+    return this.state.links.map((link) => {
+      return <p key={link._id}>{link.url}</p>
+    });
   }
   render() {
     return (
         <div>
           <p>Links List</p>
+          <div>
+            {this.renderLinksListItems()}
+          </div>
         </div>
     );
   }
