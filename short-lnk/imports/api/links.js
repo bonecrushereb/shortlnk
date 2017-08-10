a04096723c8d5bdf971644069a51b00defc6a5dc
bonecrushereb <bonecrushereb@gmail.com>
2017-08-10 10:43:56 -0700

    publication working

--- short-lnk/imports/api/links.js
+++ short-lnk/imports/api/links.js
@@ -4,7 +4,15 @@ import { Mongo } from 'meteor/mongo';
 export const Links = new Mongo.Collection('links');
 
 if (Meteor.isServer) {
-  Meteor.publish('links', () => {
-    return Links.find();
+  Meteor.publish('links', function () {
+    return Links.find({ userId: this.userId });
   });
 }
+
+Meteor.methods({
+  greetUser() {
+    console.log('greetUser is running');
+
+    return 'Hello user!';
+  }
+});
--- short-lnk/imports/ui/Link.js
+++ short-lnk/imports/ui/Link.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { withRouter } from 'react-router-dom';
 import { Accounts } from 'meteor/accounts-base';
 import { Links } from '../api/links';
@@ -16,7 +17,7 @@ export class Link extends React.Component {
     const url = this.refs.url.value.trim();
 
     if (url) {
-      Links.insert({ url });
+      Links.insert({ url, userId: Meteor.userId() });
       this.refs.url.value = '';
     }
   }
