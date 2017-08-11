ce5ed2f63361cacb827946a09cd2004cafdc4d3c
bonecrushereb <bonecrushereb@gmail.com>
2017-08-11 09:29:24 -0700

    add additional error handling and validations

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
 import { routes } from '../imports/routes/routes';
+import '../imports/startup/simple-schema-config';
 
 
 Meteor.startup(() => {
