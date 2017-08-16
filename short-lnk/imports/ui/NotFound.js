38e1c5bb4b0671c8ec67a722424a7ff626c91512
bonecrushereb <bonecrushereb@gmail.com>
2017-08-16 14:33:34 -0700

    change not found component to stateless

--- short-lnk/imports/ui/NotFound.js
+++ short-lnk/imports/ui/NotFound.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export default class NotFound extends React.Component {
-  render() {
-    return <p>NotFound component here</p>
-  }
-}
+export default () => {
+  return (
+    <div>
+      <p>Not Found component here</p>
+    </div>
+  );
+};
