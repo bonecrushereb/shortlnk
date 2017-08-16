79e2a1a79c222123e0b4e106490a951247c32a6a
bonecrushereb <bonecrushereb@gmail.com>
2017-08-16 13:12:41 -0700

    make link and privateheader  into stateless components

--- short-lnk/imports/ui/PrivateHeader.js
+++ short-lnk/imports/ui/PrivateHeader.js
@@ -3,20 +3,32 @@ import { Accounts } from 'meteor/accounts-base';
 import { withRouter } from 'react-router-dom';
 import propTypes from 'prop-types';
 
-export class PrivateHeader extends React.Component {
-  onLogout() {
-    Accounts.logout();
-    this.props.history.push('/');
-  }
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <button onClick={this.onLogout.bind(this)}>Logout</button>
-      </div>
-    );
-  }
-}
+// export class PrivateHeader extends React.Component {
+//   onLogout() {
+//     Accounts.logout();
+//     this.props.history.push('/');
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.props.title}</h1>
+//         <button onClick={this.onLogout.bind(this)}>Logout</button>
+//       </div>
+//     );
+//   }
+// }
+
+const PrivateHeader = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    <button onClick={() => {
+        Accounts.logout();
+        props.history.push('/');
+      }}>Logout</button>
+    </div>
+  );
+};
 
 PrivateHeader.propTypes = {
   title: propTypes.string.isRequired
