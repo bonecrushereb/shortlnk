648f68895e68b73b6e4e3f6daf26c2cc1fef6d94
bonecrushereb <bonecrushereb@gmail.com>
2017-08-16 14:25:48 -0700

    remove commented code

--- short-lnk/imports/ui/PrivateHeader.js
+++ short-lnk/imports/ui/PrivateHeader.js
@@ -3,21 +3,6 @@ import { Accounts } from 'meteor/accounts-base';
 import { withRouter } from 'react-router-dom';
 import propTypes from 'prop-types';
 
-// export class PrivateHeader extends React.Component {
-//   onLogout() {
-//     Accounts.logout();
-//     this.props.history.push('/');
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <h1>{this.props.title}</h1>
-//         <button onClick={this.onLogout.bind(this)}>Logout</button>
-//       </div>
-//     );
-//   }
-// }
-
 const PrivateHeader = (props) => {
   return (
     <div>
