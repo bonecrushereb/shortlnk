8649fc6e14c3d76ad951cd5bef102b4ab06b80f3
bonecrushereb <bonecrushereb@gmail.com>
2017-08-18 10:31:33 -0700

    add session to project

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,9 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
 import { routes } from '../imports/routes/routes';
+import { Session } from 'meteor/session';
 import '../imports/startup/simple-schema-config';
 
-
 Meteor.startup(() => {
+  Session.set('showVisible', true);
   ReactDOM.render(routes, document.getElementById('app'));
 });
